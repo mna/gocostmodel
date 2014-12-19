ef2fba5ccbdb83c807c632c6a53f395f8e4a7fca
Martin Angers <martin.n.angers@gmail.com>
2014-12-19 13:27:50 -0500

    add goroutine benchmarks

---
+++ goro_test.go
@@ -0,0 +1,58 @@
+package gocostmodel
+
+import (
+	"sync"
+	"testing"
+)
+
+// this one doesn't work as intended, it presumably overwhelms the scheduler
+// with tons of goros to spawn, so it ends up looking much more costly than
+// it actually is.
+func xBenchmarkGoroFireForget(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		go func() {}()
+	}
+}
+
+func BenchmarkGoroWait(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		wg := sync.WaitGroup{}
+		wg.Add(1)
+		go func() {
+			wg.Done()
+		}()
+		wg.Wait()
+	}
+}
+
+func BenchmarkGoroSend(b *testing.B) {
+	benchmarkGoroSend(b, 0)
+}
+
+func BenchmarkGoroSendBuf1(b *testing.B) {
+	benchmarkGoroSend(b, 1)
+}
+
+func BenchmarkGoroSendBuf100(b *testing.B) {
+	benchmarkGoroSend(b, 100)
+}
+
+func benchmarkGoroSend(b *testing.B, bufSize int) {
+	ch := make(chan int, bufSize)
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		for _ = range ch {
+		}
+		wg.Done()
+	}()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		ch <- i
+	}
+
+	b.StopTimer()
+	close(ch)
+	wg.Wait()
+}
